f018d9e6c016d53c80eff21295e986798c0bab4e
nickfff-dev <hunchographics@gmail.com>
2022-08-07 01:59:22 +0300

    scroller

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+
+function Home() {
+  const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
+
+  
+  return (
+    <div>
+   <div data-inviewport="scale-in"></div>
+<div data-inviewport="fade-rotate"></div>
+    </div>
+  );
+}
+
+
+export default Home;
